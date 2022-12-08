ec9026cfd9230e2596d17e078a56d429a525b3f7
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-08 13:45:22 -0700

    Remove redundant code

--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,4 @@
 import ImageHandler from './contentful-image'
-import { LINKEDIN_URL, YOUR_NAME } from '../lib/constants'
 
 export default function Intro({ profile }) {
   return (
@@ -9,8 +8,8 @@ export default function Intro({ profile }) {
           src={profile.personal.image.url}
           // layout="fill"
           className="rounded-full"
-          width={100}
-          height={100}
+          width={200}
+          height={200}
           alt={"Profile Picture"}
         />
         <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllExperiencessForHome, getProfileForHome } from '../lib/api'
 import Head from 'next/head'
-import { YOUR_NAME } from '../lib/constants'
+
 
 export default function Index({ allExperience, profile }) {
   return (
