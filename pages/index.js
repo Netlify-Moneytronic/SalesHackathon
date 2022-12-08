30076805d5682f71cfcdf432cd5e1a1f9eb6f12a
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-08 13:32:54 -0700

    Pull in profile information via the API

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Container from '../components/container'
 import ExperienceList from '../components/experience-list'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllExperiencessForHome, getProfilePic } from '../lib/api'
+import { getAllExperiencessForHome, getProfileForHome } from '../lib/api'
 import Head from 'next/head'
 import { YOUR_NAME } from '../lib/constants'
 
-export default function Index({ allExperience }) {
+export default function Index({ allExperience, profile }) {
   return (
     <>
       <Layout>
         <Head>
-          <title>{YOUR_NAME}'s Portfolio</title>
+          <title>{profile.personal.name}'s Portfolio</title>
         </Head>
         <Container>
           <Intro />
@@ -23,9 +23,9 @@ export default function Index({ allExperience }) {
 }
 
 export async function getStaticProps() {
-  // const profilePic = await getProfilePic();
+  const profile = await getProfileForHome()
   const allExperience = await getAllExperiencessForHome()
   return {
-    props: { allExperience },
+    props: { allExperience, profile },
   }
 }
