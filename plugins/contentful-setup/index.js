616a0c245b37c2e994957dc788a08b648a8fe26c
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-09 13:44:05 -0700

    Update build plugin statuses and remove setup script

--- plugins/contentful-setup/index.js
+++ plugins/contentful-setup/index.js
@@ -5,8 +5,6 @@ const { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN, URL } = process.env
 
 module.exports = {
     onPreBuild: async ({ utils }) => {
-        console.log("URL of the Site: " + URL);
-
         if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_MANAGEMENT_TOKEN) {
             throw new Error(
                 [
@@ -17,35 +15,17 @@ module.exports = {
             )
         }
 
-        console.log(`Your contentful management token is ${CONTENTFUL_MANAGEMENT_TOKEN}`);
-
         await spaceImport({
             spaceId: CONTENTFUL_SPACE_ID,
             managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
             content: exportFile,
         })
-            .then(() => console.log('The content model of your space is set up!'))
+            .then(() => console.log('The content model of your space is set up!'),
+                utils.status.show({
+                    title: "Content Loadeded Successfully",
+                    summary: "✅ Success: We've Loaded in your Pre-Built Content",
+                    text: "You're all set!",
+                }))
             .catch((e) => console.error(e))
-
-        //   try {
-        //       if (cnameCorrect && aCorrect) {
-        //         console.log("✅ Site is on Netlify High Performance Edge");
-        //         utils.status.show({
-        //           title: "HP Edge Setup Status",
-        //           summary: "✅ Success: Site is on Netlify High Performance Edge",
-        //           // Optional. Empty by default.
-        //           text: "You're all set!",
-        //         });
-        //         return;
-        //       }
-        //   } catch (error) {
-        //     console.log("🚨 WARNING: Site not on HP Edge - Check your DNS configuration");
-        //     utils.status.show({
-        //       title: "HP Edge Setup Status",
-        //       summary: "🚨 WARNING: Site not on HP Edge - Check your DNS configuration",
-        //       text: "Please contact Netlify support or your account manager to help with HP Edge setup or an upgrade.",
-        //     });
-
-        //   }
     },
 };
