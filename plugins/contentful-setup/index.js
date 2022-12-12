5b4a432c360d6a5d81d2b432f8b6fd113451aba7
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-12 15:22:29 -0700

    create check content function

--- plugins/contentful-setup/index.js
+++ plugins/contentful-setup/index.js
@@ -2,52 +2,67 @@ const spaceImport = require('contentful-import')
 const exportFile = require('./export.json')
 const contentful = require('contentful-management')
 
-const { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN } = process.env
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN, CONTENTFUL_ENVIRONMENT_ID } = process.env
 
 
 module.exports = {
     onPreBuild: async ({ utils }) => {
+
         const client = contentful.createClient({
             accessToken: CONTENTFUL_MANAGEMENT_TOKEN
         })
 
+        async function getSpacContent() {
+            return client.getSpace(CONTENTFUL_SPACE_ID)
+                .then((space) => space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID))
+                .then((environment) => environment.getContentTypes())
+                .then((response) => response.json())
+                .catch(console.error)
+        }
 
-        await client.getSpace(CONTENTFUL_SPACE_ID)
-            .then((space) => space.getEnvironment('master'))
-            .then((environment) => environment.getContentTypes())
-            .then((response) => console.log(response.items))
-            .catch(console.error)
-
-
-
-        if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_MANAGEMENT_TOKEN) {
+        if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_MANAGEMENT_TOKEN || !CONTENTFUL_ENVIRONMENT_ID) {
             throw new Error(
                 [
                     'Parameters missing...',
-                    'Please run the setup command as follows',
-                    'CONTENTFUL_SPACE_ID=XXX CONTENTFUL_MANAGEMENT_TOKEN=CFPAT-XXX npm run setup',
+                    'Please add the following environment variables to your Netlify site:',
+                    'CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN, CONTENTFUL_ENVIRONMENT_ID',
                 ].join('\n')
             )
         }
 
-        try {
-            await spaceImport({
-                spaceId: CONTENTFUL_SPACE_ID,
-                managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
-                content: exportFile,
-            })
-            utils.status.show({
-                title: "Content Loadeded Successfully",
-                summary: "✅ Success: We've Loaded in your Pre-Built Content",
-                text: "You're all set!",
-            })
-        } catch (error) {
-            utils.status.show({
-                title: "Content Load Failed",
-                summary: "❌ Error: We've Failed to Load in your Pre-Built Content",
-                text: "Please try again later",
-            })
-            throw new Error(error)
-        }
+        const spaceContent = await getSpacContent()
+
+        console.log(spaceContent);
+
+        // try {
+
+        // } catch (error) {
+        //     utils.status.show({
+        //         title: "Contentful Failed to Load",
+        //         summary: "❌ Error: We failed to connect to your contentful instance",
+        //         text: "Please check your credentials and try again later",
+        //     })
+        //     throw new Error(error)
+        // }
+
+        // try {
+        //     await spaceImport({
+        //         spaceId: CONTENTFUL_SPACE_ID,
+        //         managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
+        //         content: exportFile,
+        //     })
+        //     utils.status.show({
+        //         title: "Content Loadeded Successfully",
+        //         summary: "✅ Success: We've Loaded in your Pre-Built Content",
+        //         text: "You're all set!",
+        //     })
+        // } catch (error) {
+        //     utils.status.show({
+        //         title: "Content Load Failed",
+        //         summary: "❌ Error: We've Failed to Load in your Pre-Built Content",
+        //         text: "Please try again later",
+        //     })
+        //     throw new Error(error)
+        // }
     }
 }
