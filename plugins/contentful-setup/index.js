8d340400a6ff3be77686a875d684ab881885e461
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-12 15:02:19 -0700

    Remove netlify api logic

--- plugins/contentful-setup/index.js
+++ plugins/contentful-setup/index.js
@@ -1,39 +1,15 @@
 const spaceImport = require('contentful-import')
 const exportFile = require('./export.json')
-// const fetch = require('node-fetch');
 const contentful = require('contentful-management')
 
 const { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN } = process.env
 
 
 module.exports = {
-    onPreBuild: async ({ utils, constants }) => {
+    onPreBuild: async ({ utils }) => {
         const client = contentful.createClient({
             accessToken: CONTENTFUL_MANAGEMENT_TOKEN
         })
-        // const apiToken = NETLIFY_API_TOKEN;
-        // const teamSlug = NETLIFY_TEAM_SLUG;
-
-        // async function deleteEnvironmentVariable(envVar) {
-        //     // const url = `https://api.netlify.com/api/v1/accounts/${teamSlug}/env/${envVar}?site_id=${constants.SITE_ID}`;
-        //     const url = `https://api.netlify.com/api/v1/accounts/${teamSlug}/env?site_id=${constants.SITE_ID}`;
-        //     console.log("The URL is: " + url);
-        //     return fetch(
-        //         url,
-        //         {
-        //             // method: 'DELETE',
-        //             method: 'GET',
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //                 Authorization: `Bearer ${apiToken}`,
-        //             }
-        //         }
-        //     ).then((response) => response.json()).then((data) => {
-        //         console.log(data);
-        //         return data;
-        //     })
-        // }
-
 
 
         await client.getSpace(CONTENTFUL_SPACE_ID)
@@ -73,30 +49,5 @@ module.exports = {
             })
             throw new Error(error)
         }
-        await deleteEnvironmentVariable("NETLIFY_API_TOKEN").catch((error) => {
-            console.log(error);
-            utils.status.show({
-                title: "Failed to Delete Netlify API Token",
-                summary: "❌ Error: We've Failed to Delete your NETLIFY_API_TOKEN environment variable",
-                text: "See console for error",
-            })
-        })
-        await deleteEnvironmentVariable("NETLIFY_TEAM_SLUG").catch((error) => {
-            console.log(error);
-            utils.status.show({
-                title: "Failed to Delete NETLIFY_TEAM_SLUG",
-                summary: "❌ Error: We've Failed to Delete your NETLIFY_TEAM_SLUG environment variable",
-                text: "See console for error",
-            })
-        })
     }
-    // else {
-    //     console.log("No Content to Load");
-    //     utils.status.show({
-    //         title: "No Content to Load",
-    //         summary: "✅ Content Detected",
-    //         text: "We detected that you already have content in your space so we didn't load any content"
-    //     })
-    // }
-
 }
