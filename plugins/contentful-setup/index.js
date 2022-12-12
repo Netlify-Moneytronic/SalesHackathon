ceea67fb0263fee4a4283c76d74aeca99feaab14
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-12 16:30:17 -0700

    Debug contentful

--- plugins/contentful-setup/index.js
+++ plugins/contentful-setup/index.js
@@ -12,6 +12,11 @@ module.exports = {
             accessToken: CONTENTFUL_MANAGEMENT_TOKEN
         })
 
+        const contentfulItems = await client.getSpace(CONTENTFUL_SPACE_ID)
+            .then((space) => space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID))
+            .then((environment) => environment.getContentTypes())
+            .then((response) => console.log(response.items)).catch(console.Error)
+
 
         if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_MANAGEMENT_TOKEN || !CONTENTFUL_ENVIRONMENT_ID) {
             throw new Error(
@@ -24,41 +29,37 @@ module.exports = {
         }
 
         try {
-            await client.getSpace(CONTENTFUL_SPACE_ID)
-                .then((space) => space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID))
-                .then((environment) => environment.getContentTypes())
-                .then((response) => {
-                    const items = response.items;
-                    console.log(`We've detected ${items.length} content types in your space`);
-                    if (items.length >= 4) {
-                        utils.status.show({
-                            title: "Initial Import Skipped",
-                            summary: "✅ Content Detected",
-                            text: "We found content in your space, so we skipped adding dummy content",
-                        }
-                        )
-                    } else {
-                        try {
-                            spaceImport({
-                                spaceId: CONTENTFUL_SPACE_ID,
-                                managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
-                                content: exportFile,
-                            })
-                            utils.status.show({
-                                title: "Content Loadeded Successfully",
-                                summary: "✅ Success: We've Loaded in your Pre-Built Content",
-                                text: "You're all set!",
-                            })
-                        } catch (error) {
-                            utils.status.show({
-                                title: "Content Load Failed",
-                                summary: "❌ Error: We've Failed to Load in your Pre-Built Content",
-                                text: "Please try again later",
-                            })
-                            throw new Error(error)
-                        }
-                    }
-                }).catch(new Error("Failed to load content"))
+            console.log(`Here are your contentful items: ${contentfulItems}`)
+            // const items = response.items;
+            //         console.log(`We've detected ${items.length} content types in your space - We've skipped adding dummy content`);
+            //         if (items.length >= 4) {
+            //             utils.status.show({
+            //                 title: "Initial Import Skipped",
+            //                 summary: "✅ Content Detected",
+            //                 text: "We found content in your space, so we skipped adding dummy content",
+            //             }
+            //             )
+            //         } else {
+            //             try {
+            //                 await spaceImport({
+            //                     spaceId: CONTENTFUL_SPACE_ID,
+            //                     managementToken: CONTENTFUL_MANAGEMENT_TOKEN,
+            //                     content: exportFile,
+            //                 })
+            //                 utils.status.show({
+            //                     title: "Content Loadeded Successfully",
+            //                     summary: "✅ Success: We've Loaded in your Pre-Built Content",
+            //                     text: "You're all set!",
+            //                 })
+            //             } catch (error) {
+            //                 utils.status.show({
+            //                     title: "Content Load Failed",
+            //                     summary: "❌ Error: We've Failed to Load in your Pre-Built Content",
+            //                     text: "Please try again later",
+            //                 })
+            //                 throw new Error(error)
+            //             }
+            //         }
         } catch (error) {
             utils.status.show({
                 title: "Contentful Failed to Load",
