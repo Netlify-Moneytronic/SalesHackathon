5f35264e14739797c77785b38b5dd9cc3a2f586b
Sean Mc Ternan <seanmcternan@gmail.com>
2022-12-13 11:15:31 -0700

    remove debigging log

--- plugins/contentful-setup/index.js
+++ plugins/contentful-setup/index.js
@@ -16,7 +16,6 @@ module.exports = {
             const space = await client.getSpace(CONTENTFUL_SPACE_ID)
             const environment = await space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID)
             const response = await environment.getContentTypes()
-            console.log(response);
             try {
                 return response.items
             } catch (error) {
